3a3b9ca9247a08d17a271a36759e9375e46bb586
Maruseu <marceucamp@gmail.com>
2020-03-30 12:20:58 -0300

    no bat fix

--- scripts/status/blocks/6bat.sh
+++ scripts/status/blocks/6bat.sh
@@ -4,33 +4,36 @@ cat >> ddblocks << EOF
 
 uBat () {
 	for i in /sys/class/power_supply/BAT?/ ; do
-		bat=\`cat \$i/capacity\`
-		if [ \$bat -lt 11 ]; then
-			bat="░"
-		elif [ \$bat -lt 22 ]; then
-			bat="▁"
-		elif [ \$bat -lt 33 ]; then
-			bat="▂"
-		elif [ \$bat -lt 44 ]; then
-			bat="▃"	
-		elif [ \$bat -lt 55 ]; then
-			bat="▄"
-		elif [ \$bat -lt 67 ]; then
-			bat="▅"
-		elif [ \$bat -lt 78 ]; then
-			bat="▆"
-		elif [ \$bat -lt 89 ]; then
-			bat="▇"
-		else
-			bat="█"
-		fi
-		
-		if grep 'Discharging' \$i/status > /dev/null; then
-			tbat="\$ci ▕\$bat▏ "
-		else
-			tbat="\$cs ▕\$bat▏ "
+		if [ -e \$i ]; then
+			bat=\`cat \$i/capacity\`
+			if [ \$bat -lt 11 ]; then
+				bat="░"
+			elif [ \$bat -lt 22 ]; then
+				bat="▁"
+			elif [ \$bat -lt 33 ]; then
+				bat="▂"
+			elif [ \$bat -lt 44 ]; then
+				bat="▃"	
+			elif [ \$bat -lt 55 ]; then
+				bat="▄"
+			elif [ \$bat -lt 67 ]; then
+				bat="▅"
+			elif [ \$bat -lt 78 ]; then
+				bat="▆"
+			elif [ \$bat -lt 89 ]; then
+				bat="▇"
+			else
+				bat="█"
+			fi
+			
+			if grep 'Discharging' \$i/status > /dev/null; then
+				tbat="\$ci ▕\$bat▏ "
+			else
+				tbat="\$cs ▕\$bat▏ "
+			fi
 		fi
 	done
+	[ -z "\$tbat" ] && tbat="\$cs"
 	updateRoot=1
 }
 iBat=1
